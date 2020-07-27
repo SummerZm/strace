66fcce1e8f1a7730252bda9e3550f444533d60be
Ákos Uzonyi <uzonyi.akos@gmail.com>
2020-07-27 20:54:08 +0200

    Use get_proc_pid for /proc paths
    * defs.h (get_proc_pid): New function definition.
    * mmap_cache.c (mmap_cache_rebuild_if_invalid): Use proc pid instead of
    tcp->pid for /proc path.
    * util.c (getfdproto): Likewise.
    (pidfd_get_pid): Likewise.
    * pathtrace.c (getfdpath_pid): Likewise.
    * strace.c (attach_tcb): Likewise.

--- defs.h
+++ defs.h
@@ -1002,7 +1002,7 @@ extern kernel_ulong_t *
 fetch_indirect_syscall_args(struct tcb *, kernel_ulong_t addr, unsigned int n_args);
 
 extern void pidns_init(void);
-
+extern int get_proc_pid(struct tcb *tcp);
 extern int translate_pid(struct tcb *tcp, int dest_id, enum pid_type type,
 		    int *proc_pid_ptr);
 
